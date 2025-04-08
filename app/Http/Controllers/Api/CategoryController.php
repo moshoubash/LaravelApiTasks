c3417207daf443723b4abb25166696f358cdcb5d
Mohammad Shoubash <mohammedshobash2002@gmail.com>
2025-04-08 16:26:52 +0300

    Add Category CRUD

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Category::create($request->all());
+        return json_decode($request->all);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::find($id);
+        $category->name = $request->name;
+        $category->save();
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        
+        return json_encode($category);
+    }
+}
